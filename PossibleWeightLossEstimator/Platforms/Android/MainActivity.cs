9d68cd5e9cef6e0f7135e814870db05966ecd7d1
modigida <modigida@hotmail.com>
2024-11-01 08:57:21 +0100

    Customize color on statusbar

--- PossibleWeightLossEstimator/Platforms/Android/MainActivity.cs
+++ PossibleWeightLossEstimator/Platforms/Android/MainActivity.cs
@@ -1,11 +1,23 @@
 ﻿using Android.App;
 using Android.Content.PM;
 using Android.OS;
+using Android.Views;
 
 namespace PossibleWeightLossEstimator
 {
     [Activity(Theme = "@style/Maui.SplashTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize | ConfigChanges.Density)]
     public class MainActivity : MauiAppCompatActivity
     {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            if (Build.VERSION.SdkInt >= BuildVersionCodes.Lollipop)
+            {
+                Window.DecorView.SystemUiVisibility = (StatusBarVisibility)SystemUiFlags.LightStatusBar;
+
+                Window.SetStatusBarColor(Android.Graphics.Color.ParseColor("#F5F5F5"));
+            }
+        }
     }
 }
